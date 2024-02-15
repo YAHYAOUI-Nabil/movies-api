d8e4df800b2ecb190452a02ef6d4c7e6638f06e6
Nabil Yahyaoui <yahyaouinabil1993@gmail.com>
2024-02-15 15:14:49 +0100

    create review controller service and repository

---
+++ src/main/java/com/movies/api/repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.movies.api.repository;
+
+import com.movies.api.model.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
